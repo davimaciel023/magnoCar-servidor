3f221110dab3e512b03497c57265c58f2e309362
Davi Maciel <davimaciel077@gmail.com>
2025-01-27 11:33:50 -0300

    atualizando as paginas de navegação e adicionando imagens nas pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+import { HomeComponent } from './componente/pages/home/home.component';
+import { SobreComponent } from './componente/pages/sobre/sobre.component';
+import { ProdutosComponent } from './componente/pages/produtos/produtos.component';
+import { ContatoComponent } from './componente/pages/contato/contato.component';
+import { RodapeComponent } from './componente/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    HomeComponent,
+    SobreComponent,
+    ProdutosComponent,
+    ContatoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
