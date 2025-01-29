446acddfe69368eba180bc710fe1ca8ff1a5ae26
Tales Ramos <araujotales02@gmail.com>
2025-01-29 13:19:33 -0300

    home css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { AppRoutingModule } from './app-routing.module';
+  import { AppComponent } from './app.component';
+  import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+  import { HomeComponent } from './componente/pages/home/home.component';
+  import { SobreComponent } from './componente/pages/sobre/sobre.component';
+  import { ProdutosComponent } from './componente/pages/produtos/produtos.component';
+  import { ContatoComponent } from './componente/pages/contato/contato.component';
+  import { RodapeComponent } from './componente/rodape/rodape.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
-import { HomeComponent } from './componente/pages/home/home.component';
-import { SobreComponent } from './componente/pages/sobre/sobre.component';
-import { ProdutosComponent } from './componente/pages/produtos/produtos.component';
-import { ContatoComponent } from './componente/pages/contato/contato.component';
-import { RodapeComponent } from './componente/rodape/rodape.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    CabecalhoComponent,
-    HomeComponent,
-    SobreComponent,
-    ProdutosComponent,
-    ContatoComponent,
-    RodapeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  @NgModule({
+    declarations: [
+      AppComponent,
+      CabecalhoComponent,
+      HomeComponent,
+      SobreComponent,
+      ProdutosComponent,
+      ContatoComponent,
+      RodapeComponent
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
