de5d1296618833aece30ada81b9e969409212d4e
Davi Maciel <davimaciel077@gmail.com>
2025-01-29 10:22:11 -0300

    atualização de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componente/rodape/rodape.component';
+import { HomeComponent } from './componente/pages/home/home.component';
+import { SobreComponent } from './componente/pages/sobre/sobre.component';
+import { ProdutosComponent } from './componente/pages/produtos/produtos.component';
+import { ContatoComponent } from './componente/pages/contato/contato.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: ''
+    redirectTo: 'cabecalho',
+    pathMatch: 'full'
+  },
+  {
+    path: 'cabecalho',
+    component: CabecalhoComponent
+  },
+  {
+    path: 'rodape',
+    component: RodapeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  },
+  {
+    path: 'produtos',
+    component: ProdutosComponent
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent
   }
 ];
 
